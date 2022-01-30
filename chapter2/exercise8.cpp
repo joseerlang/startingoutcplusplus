5b5d0118ee3fc3721b0b2ff25b664ad1c542e697
JosePersonal <jose@mestre0184.mestrelab.local>
2022-01-30 21:31:41 +0100

    Exercise 8 done

---
+++ chapter2/exercise8.cpp
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2022, joseerlang <joseerlang@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+//Is the variable name Sales the same as sales ? Why or why not?
+// No is the same, Sales and sales are 2 different variables, because are case sensitive.
