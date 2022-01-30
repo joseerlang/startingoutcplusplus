56e05e5214e70129e58257bfcc5139a441986d7e
JosePersonal <jose@mestre0184.mestrelab.local>
2022-01-30 21:28:56 +0100

    Exercise 7 done

---
+++ chapter2/exercise7.cpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2022, joseerlang <joseerlang@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+/*
+Which of the following are illegal variable names, and why?
+X
+99bottles
+july97
+theSalesFigureForFiscalYear98
+r&d  ***"Illegal"
+grade_report
+*/
