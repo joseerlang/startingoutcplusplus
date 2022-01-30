a6871c593710709192a8c2bc81a4ef02277343a4
JosePersonal <jose@mestre0184.mestrelab.local>
2022-01-30 21:13:28 +0100

    Exercise4 done

---
+++ chapter2/exercise4.cpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2022, joseerlang <joseerlang@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+//Today's date 30 - 01 - 2020
+#include "iostream"
+
+using namespace std;
+
+int main() {
+    cout << "First Name" << endl;
+    cout << "Street and number" << endl;
+    cout << "City State and Zip code" << endl;
+    cout << "Telephone number " << endl;
+    return 0;
+}
