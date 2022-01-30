20493df62d754d4197eb5b1eadeed0c5f3dd5c11
JosePersonal <jose@mestre0184.mestrelab.local>
2022-01-30 20:36:27 +0100

    Added checkpoint 2 , problem 2

---
+++ chapter2/check2exercise2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Success\n";
+    cout << "Success";
+    cout << " Success\n\n";
+    cout << "Success\n";
+    return 0;
+}
