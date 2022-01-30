8245e2b36a62980b810042bd5df454f222b085f4
JosePersonal <jose@mestre0184.mestrelab.local>
2022-01-30 21:21:20 +0100

    Exercise 6 done

---
+++ chapter2/exercise6.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2022, joseerlang <joseerlang@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    int number;
+    number = 712;
+    cout << "The value is " << "number" << endl;
+    return 0;
+}*/
+// solution:
+// The value is number
