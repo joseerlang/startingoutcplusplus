af8bd22f811b89d5073506d4db60763dcc54ab22
JosePersonal <jose@mestre0184.mestrelab.local>
2022-01-30 21:35:14 +0100

    Exercise 9 done

---
+++ chapter2/exercise9.cpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2022, joseerlang <joseerlang@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+/*
+Refer to the data types listed in Table 2-6 for these questions.
+A)	If a variable needs to hold numbers in the range 32 to 6,000, what data type
+        would be best?
+B)	If a variable needs to hold numbers in the range 240,000 to 140,000, what
+        data type would be best?
+C) Which of the following literals uses more memory? 20 or 20L
+ */
+
+// A) short int
+// B) unsigned int
+// C) 20 because 20L represents a long literal
