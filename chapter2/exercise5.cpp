45faa677af79a6ffc6e7a6f9dc1ea595392b9e79
JosePersonal <jose@mestre0184.mestrelab.local>
2022-01-30 21:19:50 +0100

    Exercise5 done

---
+++ chapter2/exercise5.cpp
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2022, joseerlang <joseerlang@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+// This program uses variables and literals.
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    int little;
+    int big;
+    little = 2;
+    big = 2000;
+    cout << "The little number is " << little << endl;
+    cout << "The big number is " << big << endl;
+    return 0;
+}*/
+//List all the variables and literals that appear in the program.
+//Variables: little, big,
+//Literals: 2, 2000, The little number is, The big number is, 0
