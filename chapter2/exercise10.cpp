26fcb87f18d5a71d224556daf14fa6758be83472
JosePersonal <jose@mestre0184.mestrelab.local>
2022-01-30 21:36:34 +0100

    Exercise 10 done

---
+++ chapter2/exercise10.cpp
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2022, joseerlang <joseerlang@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+//On any computer, which data type uses more memory, an integer or an unsigned
+//integer?
+// The same because both types use 4 bytes to store values.
