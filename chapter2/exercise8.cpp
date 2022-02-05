367b52af148fe22ce8962c514305b62de9032c9f
JosePersonal <jose@mestre0184.mestrelab.local>
2022-02-05 17:28:26 +0100

    Removed references to book

--- chapter2/exercise8.cpp
+++ chapter2/exercise8.cpp
@@ -4,5 +4,4 @@
  * SPDX-License-Identifier: BSD-2-Clause
 */
 
-//Is the variable name Sales the same as sales ? Why or why not?
 // No is the same, Sales and sales are 2 different variables, because are case sensitive.
