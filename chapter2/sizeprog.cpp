4986e82a93cf0c81065801aab637ff74e8813eff
jose <micerinos>
2022-02-05 19:11:03 +0100

    new exercises

---
+++ chapter2/sizeprog.cpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2022, joseerlang <joseerlang@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+#include "iostream"
+using namespace  std;
+
+int main()
+{
+    long double apple;
+    int value1= 4.9;
+    int value2= 6.2;
+    cout << "The size of an integer is "<< sizeof(int);
+    cout << " bytes.\n";
+    cout << "The size of an long integer is "<< sizeof(long);
+    cout << " bytes.\n";
+    cout << "The size of an long double  is "<< sizeof(apple);
+    cout << " bytes.\n";
+
+    cout <<  70 / 3;
+
+}
